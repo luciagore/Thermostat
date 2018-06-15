64f6f52046f6e2e109f7d4aef302acfdef0c7017
luciagore <lucy.gore@hotmail.co.uk>
2018-06-15 14:11:58 +0100

    tidying up index and interface to show inside and outside temperatures and refactoring interface

--- src/interface.js
+++ src/interface.js
@@ -1,46 +1,56 @@
 $(document).ready(function() {
 
-  $('#current-city').change(function() {
+  displayWeather('London');
+
+  $('#select-city').submit(function(event) {
+    event.preventDefault();
     var city = $('#current-city').val();
-    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
-      $('#current-temperature').text(data.main.temp)
-    })
+    displayWeather(city);
   })
-  
+
   var thermostat = new Thermostat();
   $('#temperature').text(thermostat.temperature);
 
 
-$('#temp-up').click(function() {
-  thermostat.up();
-  updateTemperature();
-});
+  $('#temp-up').click(function() {
+    thermostat.up();
+    updateTemperature();
+  });
 
-$('#temp-down').click(function() {
-  thermostat.down();
-  updateTemperature();
-});
+  $('#temp-down').click(function() {
+    thermostat.down();
+    updateTemperature();
+  });
 
-$('#temp-reset').click(function() {
-  thermostat.reset();
-  updateTemperature();
-});
+  $('#temp-reset').click(function() {
+    thermostat.reset();
+    updateTemperature();
+  });
 
-$('#psm-on').click(function() {
-  thermostat.powersavingon();
-  $('#power-saving').text('on')
-  updateTemperature();
-})
+  $('#psm-on').click(function() {
+    thermostat.powersavingon();
+    $('#power-saving').text('on')
+    updateTemperature();
+  })
 
-$('#psm-off').click(function() {
-  thermostat.powersavingoff();
-  $('#power-saving').text('off')
-  updateTemperature();
-})
+  $('#psm-off').click(function() {
+    thermostat.powersavingoff();
+    $('#power-saving').text('off')
+    updateTemperature();
+  })
 
-})
+  })
 
-function updateTemperature() {
-  $('#temperature').text(thermostat.temperature);
-  $('#temperature').attr('class', thermostat.usage());
-}
+  function displayWeather(city) {
+   var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+   var token = '&appid=a3d9eb01d4de82b9b8d0849ef604dbed';
+   var units = '&units=metric';
+   $.get(url + token + units, function(data) {
+     $('#current-temperature').text(data.main.temp);
+   })
+  }
+
+  function updateTemperature() {
+    $('#temperature').text(thermostat.temperature);
+    $('#temperature').attr('class', thermostat.usage());
+  }
