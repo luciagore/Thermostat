3b3d7e0a7fcb26e75cc3ba25f415baa03cefaad0
luciagore <lucy.gore@hotmail.co.uk>
2018-06-15 11:36:06 +0100

    adding in weather api

--- src/interface.js
+++ src/interface.js
@@ -1,7 +1,15 @@
 $(document).ready(function() {
+
+  $('#current-city').change(function() {
+    var city = $('#current-city').val();
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+      $('#current-temperature').text(data.main.temp)
+    })
+  })
+  
   var thermostat = new Thermostat();
   $('#temperature').text(thermostat.temperature);
-})
+
 
 $('#temp-up').click(function() {
   thermostat.up();
@@ -28,6 +36,8 @@ $('#psm-off').click(function() {
   thermostat.powersavingoff();
   $('#power-saving').text('off')
   updateTemperature();
+})
+
 })
 
 function updateTemperature() {
